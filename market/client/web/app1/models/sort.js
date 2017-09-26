bdd7ab75c55f0624ada1551d328cb67d7c1f2fa2
Alex Kot <akotlyachkov@gmail.com>
2017-09-27 01:55:08 +0300

    удаление лишних файлов

--- market/client/web/app1/models/sort.js
+++
@@ -1,8 +0,0 @@
-export var SortingMode;
-(function (SortingMode) {
-    SortingMode[SortingMode["Asc"] = 1] = "Asc";
-    SortingMode[SortingMode["Desc"] = -1] = "Desc";
-})(SortingMode || (SortingMode = {}));
-export class Sort {
-}
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic29ydC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInNvcnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsTUFBTSxDQUFOLElBQVksV0FHWDtBQUhELFdBQVksV0FBVztJQUNuQiwyQ0FBTyxDQUFBO0lBQ1AsOENBQVMsQ0FBQTtBQUNiLENBQUMsRUFIVyxXQUFXLEtBQVgsV0FBVyxRQUd0QjtBQUVELE1BQU07Q0FLTCIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBlbnVtIFNvcnRpbmdNb2RlIHtcclxuICAgIEFzYyA9IDEsXHJcbiAgICBEZXNjID0gLTFcclxufVxyXG5cclxuZXhwb3J0IGNsYXNzIFNvcnR7XHJcbiAgICBuYW1lOiBzdHJpbmc7XHJcbiAgICBmaWVsZDogc3RyaW5nO1xyXG4gICAgbW9kZTogbnVtYmVyO1xyXG4gICAgYWN0aXZlOiBib29sZWFuXHJcbn0iXX0=
